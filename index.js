bf7d8ca32c4f1386fc06f19b78f42aeb30b89e92
Kislay Gupta <kislaygupta007@gmail.com>
2025-08-12 14:45:21 +0530

    feat: api docs and better file structure

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ import express, { json } from "express";
 import cors from "cors";
 import router from "./routes/routes.js";
 import "dotenv/config";
+import path from "path";
+
 const app = express();
 app.use(cors());
 app.use(json());
+// Serve API docs HTML page
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(process.cwd(), "./public/api-docs.html"));
+});
+
+//api endpoints
 app.use("/api/v1", router);
 
 const PORT = process.env.PORT || 4000;
